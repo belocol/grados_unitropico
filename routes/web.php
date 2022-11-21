4e431ad9d13cf8d929e5820af09ccfac7ba90dff
Fabian Guevara <fabianguevara.es@unitropico.edu.co>
2022-11-20 23:13:26 -0500

    se añaden los controladores al archivo a los rutas del web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Aprobacion_procesoController;
+use App\Http\Controllers\BarrioController;
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\DependenciaController;
+use App\Http\Controllers\DocumentoController;
+use App\Http\Controllers\EgresadoController;
+use App\Http\Controllers\FacultadController;
+use App\Http\Controllers\Historia_notaController;
+use App\Http\Controllers\Informacion_gradoController;
+use App\Http\Controllers\Modalidad_gradoController;
+use App\Http\Controllers\MunicipioController;
+use App\Http\Controllers\Nivel_estudioController;
+use App\Http\Controllers\NotificacionController;
+use App\Http\Controllers\PaisController;
+use App\Http\Controllers\ProcesoController;
+use App\Http\Controllers\Programa_academicoController;
+use App\Http\Controllers\Tipo_documentoController;
 
 /*
 |--------------------------------------------------------------------------
